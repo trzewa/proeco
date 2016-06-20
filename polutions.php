a5a58584a890fbd228345d0f0b5b3bc8cac464cf
Krzysztof <trzewa@wp.pl>
2016-06-20 23:34:29 +0200

    Proba_final

--- polutions.php
+++ polutions.php
@@ -5,28 +5,49 @@
     <title>Dane zanieczyszczen</title>
   </head>
   <body>
-    <a href="#" id="get-data">Get JSON data</a>
-    <div id="show-data"></div>
+   
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 
 <?php
-$url = "http://powietrze.malopolska.pl/_powietrzeapi/api/dane?act=danemiasta&ci_id=02";
+$first;
+$count;
+for($id=$first;$id<=$count;$id++)
+{
+$url = "http://powietrze.malopolska.pl/_powietrzeapi/api/dane?act=danemiasta&ci_id=".$id;
 $json = file_get_contents($url);
 $json_data = json_decode($json, JSON_PRETTY_PRINT);
 //echo "My token: ". var_dump($json_data);
-	echo("dane dla miasta: ".$json_data['dane']['city']["ci_countydesc"]);
+	echo("Dane dla miasta: ".$json_data['dane']['city']["ci_city"]);
+	echo("<br>");	
+	echo("Data pomiaru: ".$json_data['dane']['forecast']['dzisiaj']['averages'][0]['cf_date']);
+	echo("<br>");	
+	echo ("<strong>Nazwa wskaźnika: ".$json_data['dane']['forecast']['dzisiaj']['details'][0]['fo_wskaznik']);
+	echo ("</strong>");
 	echo("<br>");
-	echo("dane ze stacji: ".$json_data['dane']['actual'][0]['station_name']);
+	echo ("<b>Wartość: ".$json_data['dane']['forecast']['dzisiaj']['details'][1]['fo_wartosc']);
+	echo ("</b>");
+	echo("<br>");	
+	echo ("<strong>Nazwa wskaźnika: ".$json_data['dane']['forecast']['dzisiaj']['details'][1]['fo_wskaznik']);
+	echo ("</strong>");
 	echo("<br>");
-	echo $json_data['dane']['actual'][0]['details'][0]['g_nazwa'];
+	echo ("<b>Wartość: ".$json_data['dane']['forecast']['dzisiaj']['details'][1]['fo_wartosc']);
+	echo ("</b>");
 	echo("<br>");	
-	echo $json_data['dane']['actual'][0]['details'][0]['par_name'];
+	echo ("<strong>Nazwa wskaźnika: ".$json_data['dane']['forecast']['dzisiaj']['details'][2]['fo_wskaznik']);
+	echo ("</strong>");
+	echo("<br>");
+	echo ("<b>Wartość: ".$json_data['dane']['forecast']['dzisiaj']['details'][2]['fo_wartosc']);
+	echo ("</b>");
 	echo("<br>");	
-	echo $json_data['dane']['actual'][0]['details'][0]['par_desc'];
-	echo("<br>");	
-	echo $json_data['dane']['actual'][0]['details'][0]['par_unit'];
-	echo("<br>");	
-	echo $json_data['dane']['actual'][0]['details'][0]['par_html'];
+	echo ("<strong>Nazwa wskaźnika: ".$json_data['dane']['forecast']['dzisiaj']['details'][2]['fo_wskaznik']);
+	echo ("</strong>");
+	echo("<br>");
+	echo ("<b>Wartość: ".$json_data['dane']['forecast']['dzisiaj']['details'][2]['fo_wartosc']);
+	echo ("</b>");
+	echo("<br>");
+	echo("<br>");
+}
+
 ?>
   </body>
 </html>
